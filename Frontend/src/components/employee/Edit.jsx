16050e2365ccf345bcefce401c10a6f5bd67ed2e
Pankaj kumar <99684150+Pankaj-26@users.noreply.github.com>
2025-01-21 23:25:02 +0530

    Update Edit.jsx

--- Frontend/src/components/employee/Edit.jsx
+++ Frontend/src/components/employee/Edit.jsx
@@ -40,7 +40,7 @@ const Edit = () => {
     const fetchEmployee = async () => {
         try {
           const response = await axios.get(
-            `http://localhost:5000/api/employee/${id}`,
+            `https://ems-server-i6vf.onrender.com/api/employee/${id}`,
             {
               headers: {
                 Authorization: `Bearer ${localStorage.getItem("token")}`,
